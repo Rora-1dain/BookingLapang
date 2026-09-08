a1a51664128e17be4dbf1678c802cfd7bfe5a703
Rora-1dain <pnpn87352@gmail.com>
2026-09-08 19:36:45 +0700

    fix

--- booking-lapang/app/Http/Controllers/AdminPayoutController.php
+++ booking-lapang/app/Http/Controllers/AdminPayoutController.php
@@ -28,8 +28,10 @@ class AdminPayoutController extends Controller
 
         try {
             $pemilik = User::findOrFail($validated['pemilik_id']);
-            $payoutService->buatPayout(
-                $pemilik, $validated['periode_mulai'], $validated['periode_selesai']
+           $payoutService->buatPayout(
+            $pemilik,
+            \Carbon\Carbon::parse($validated['periode_mulai']),
+            \Carbon\Carbon::parse($validated['periode_selesai'])
             );
 
             return back()->with('success', 'Payout berhasil dibuat.');
