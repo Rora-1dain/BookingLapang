709fef1a911fb85d3299fc0f461ef531c38f41f2
Xyira <xyiraaaira@gmail.com>
2026-09-08 16:51:25 +0700

    ardan hari 2

--- booking-lapang/app/Models/Booking.php
+++ booking-lapang/app/Models/Booking.php
@@ -8,26 +8,24 @@ class Booking extends Model
     use HasFactory;
     protected $table = 'bookings';
     protected $fillable = [
-        'user_id',
-        'lapangan_id',
-        'tanggal_booking',
-        'jam_mulai',
-        'jam_selesai',
-        'total_harga',
-        'status',
-        'status_refund',
-        'alasan_pembatalan',
-        'catatan_refund',
-        'metode_pembayaran',
-        'status_pembayaran',
-        'payment_reference',
-        'voucher_id',
-        'total_diskon',
-        'nomor_invoice',
-        'status_refund',
-        'alasan_pembatalan',
-        'catatan_refund',
-    ];
+    'user_id',
+    'lapangan_id',
+    'tanggal_booking',
+    'jam_mulai',
+    'jam_selesai',
+    'total_harga',
+    'status',
+    'status_refund',
+    'alasan_pembatalan',
+    'catatan_refund',
+    'metode_pembayaran',
+    'status_pembayaran',
+    'payment_reference',
+    'voucher_id',
+    'total_diskon',
+    'nominal_komisi',        // ← baris baru
+    'pendapatan_pemilik',    // ← baris baru
+   ];
     protected $casts = [
         'tanggal_booking' => 'date',
     ];
