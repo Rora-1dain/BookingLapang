e38f20d58453d7c4040a0988487eab52604324d8
Xyira <xyiraaaira@gmail.com>
2026-09-16 13:10:50 +0700

    feat: model paket booking berulang (Ardan)

---
+++ booking-lapang/app/Models/RecurringBooking.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RecurringBooking extends Model
+{
+    protected $fillable = [
+        'user_id', 'lapangan_id', 'hari', 'jam_mulai', 'jam_selesai',
+        'tanggal_mulai', 'jumlah_sesi', 'status',
+    ];
+
+    protected $casts = ['tanggal_mulai' => 'date'];
+
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
+
+    public function lapangan()
+    {
+        return $this->belongsTo(Lapangan::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
